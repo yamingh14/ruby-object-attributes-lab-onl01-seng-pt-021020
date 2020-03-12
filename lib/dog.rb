6304b7b80505ddf3a8e780e38df446c8d0c123f2
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-12 02:11:11 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(modality)
+    @breed = modality
+  end
+
+  def breed
+    @breed
+  end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def name=(naming)
+    @name = naming
+  end
+
+  def name
+    @name
+  end
+
+  def job=(task)
+    @job = task
+  end
+
+  def job
+    @job
+  end
+
+end
